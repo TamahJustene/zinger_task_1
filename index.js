e885b27d97fe8fbd6983315f476d2e49fd74d799
TAMAH JUSTENE <tamahjustin123@gmail.come>
2023-08-02 10:55:45 +0100

    The UI has been adjusted and looking appealing.

--- index.js
+++ index.js
@@ -6,21 +6,19 @@ $(document).ready(function () {
     $("#mainTodoList").empty();
     mainTodoList.forEach((todo, index) => {
       const parentTodo = `
-        <div class="card mb-3">
+        <div class="card  transparent ">
           <div class="card-body row flex flex-wrap">
             <div class="col-md-7">
               <h5 class="card-title">${todo.title}</h5>
             </div>
-            <div class="col-md-5">
-              <button class="btn btn-sm mr-2 btn-add-child-todo" data-toggle="modal" data-target="#childTodoModal"
-                data-index="${index}">
+            <div class="col-md-5 text-end">
+              <button class="btn btn-sm mr-2 btn-add-child-todo" data-toggle="modal" data-target="#childTodoModal" data-index="${index}">
                 <i class="fa-thin fa-arrow-turn-down-right"></i>+
               </button>
               <button class="btn btn-sm mr-2 delete-todo" data-index="${index}">
                 <i class="fa fa-trash"></i>
               </button>
-              <button class="btn btn-sm mr-2 edit-todo" data-toggle="modal" data-target="#mainTodoModal"
-                data-index="${index}">
+              <button class="btn btn-sm mr-2 edit-todo" data-toggle="modal" data-target="#mainTodoModal" data-index="${index}">
                 <i class="fa fa-pencil"></i>
               </button>
               <button class="btn btn-sm tick-todo" data-index="${index}">
@@ -35,24 +33,38 @@ $(document).ready(function () {
 
       // Check if there are child todos
       if (todo.children.length > 0) {
-        const childTodoCard = `
-          <div class="card mb-3 child-todo-card bg-transparent">
-            <div class="card-body">
-              ${todo.children.map((child, childIndex) => `
-                <div class="card mb-4 child-todo">
-                  <div class="card-body">
-                    <h6 class="card-title">${child.title}</h6>
-                  </div>
+        const childTodoList = $("<div class='ml-3 mt-2'></div>"); // Add left margin and top margin
+        todo.children.forEach((child) => {
+          const childTodo = `
+            <div class="card  transparent ml-3 offset-md-1">
+              <div class="card-body row flex flex-wrap">
+                <div class="col-md-7">
+                  <h6 class="card-title">${child.title}</h6>
                 </div>
-              `).join("")}
+                <div class="col-md-5 text-end">
+                <button class="btn btn-sm mr-2 btn-add-child-todo" data-toggle="modal" data-target="#childTodoModal" data-index="${index}">
+                <i class="fa-thin fa-arrow-turn-down-right"></i>+
+              </button>
+              <button class="btn btn-sm mr-2 delete-todo" data-index="${index}">
+                <i class="fa fa-trash"></i>
+              </button>
+              <button class="btn btn-sm mr-2 edit-todo" data-toggle="modal" data-target="#mainTodoModal" data-index="${index}">
+                <i class="fa fa-pencil"></i>
+              </button>
+              <button class="btn btn-sm tick-todo" data-index="${index}">
+                <i class="fa fa-check"></i>
+              </button>
+                </div>
+              </div>
             </div>
-          </div>
-        `;
-
-        $("#mainTodoList").append(childTodoCard);
+          `;
+          childTodoList.append(childTodo);
+        });
+        $("#mainTodoList").append(childTodoList);
       }
     });
   }
+
 
   function showTodoDetails(index) {
     const todo = mainTodoList[index];
@@ -61,8 +73,18 @@ $(document).ready(function () {
         <div class="card-body">
           <h5 class="card-title">Details:</h5>
           <p class="card-text">${todo.description}</p>
-          <p class="card-text"><strong>Start Date:</strong> ${todo.startDate}</p>
-          <p class="card-text"><strong>End Date:</strong> ${todo.endDate}</p>
+          
+          <div class="row">
+          <div class="col-md-6">
+            <p class="card-text"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512" fill="green"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 32C0 14.3 14.3 0 32 0H64 320h32c17.7 0 32 14.3 32 32s-14.3 32-32 32V75c0 42.4-16.9 83.1-46.9 113.1L237.3 256l67.9 67.9c30 30 46.9 70.7 46.9 113.1v11c17.7 0 32 14.3 32 32s-14.3 32-32 32H320 64 32c-17.7 0-32-14.3-32-32s14.3-32 32-32V437c0-42.4 16.9-83.1 46.9-113.1L146.7 256 78.9 188.1C48.9 158.1 32 117.4 32 75V64C14.3 64 0 49.7 0 32zM96 64V75c0 25.5 10.1 49.9 28.1 67.9L192 210.7l67.9-67.9c18-18 28.1-42.4 28.1-67.9V64H96zm0 384H288V437c0-25.5-10.1-49.9-28.1-67.9L192 301.3l-67.9 67.9c-18 18-28.1 42.4-28.1 67.9v11z"/></svg> ${todo.startDate}</p>
+          </div>
+        
+          <div class="col-md-6">
+            <p class="card-text"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512" fill="red"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 32C0 14.3 14.3 0 32 0H64 320h32c17.7 0 32 14.3 32 32s-14.3 32-32 32V75c0 42.4-16.9 83.1-46.9 113.1L237.3 256l67.9 67.9c30 30 46.9 70.7 46.9 113.1v11c17.7 0 32 14.3 32 32s-14.3 32-32 32H320 64 32c-17.7 0-32-14.3-32-32s14.3-32 32-32V437c0-42.4 16.9-83.1 46.9-113.1L146.7 256 78.9 188.1C48.9 158.1 32 117.4 32 75V64C14.3 64 0 49.7 0 32zM96 64V75c0 25.5 10.1 49.9 28.1 67.9L192 210.7l67.9-67.9c18-18 28.1-42.4 28.1-67.9V64H96zm0 384H288V437c0-25.5-10.1-49.9-28.1-67.9L192 301.3l-67.9 67.9c-18 18-28.1 42.4-28.1 67.9v11z"/></svg> ${todo.endDate}</p>
+          </div>
+        </div>
+        
+
         </div>
       </div>
     `;
